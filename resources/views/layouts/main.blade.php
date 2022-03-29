9d32764d481450a979d83c1ff9900e0a35963870
Carina Viana <carina.viana@etec.sp.gov.br>
2022-03-29 10:44:04 -0300

    primeiro commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,42 +17,60 @@
         <script src="/js/scripts.js"></script>
         </head>
         <body>
-        <header>
+      <header>
         <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="collapse navbar-collapse" id="navbar">
+          <div class="collapse navbar-collapse" id="navbar">
             <a href="/" class="navbar-brand">
-            <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
             </a>
             <ul class="navbar-nav">
-            <li class="nav-item">
+              <li class="nav-item">
                 <a href="/" class="nav-link">Eventos</a>
-            </li>
-            <li class="nav-item">
+              </li>
+              <li class="nav-item">
                 <a href="/events/create" class="nav-link">Criar Eventos</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link">Entrar</a>
-            </li>
-            <li class="nav-item">
-                <a href="/" class="nav-link">Cadastrar</a>
-            </li>
+              </li>
+              @auth <!--se está atenticado aparece os eventos do usuário logado-->
+              <li class="nav-item">
+                <a href="/dashboard" class="nav-link">Meus eventos</a>
+              </li>
+              <li class="nav-item">
+                <form action="/logout" method="POST"><!--quando logout-->
+                  @csrf
+                  <a href="/logout"
+                    class="nav-link"
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    Sair
+                  </a>
+                </form>
+              </li>
+              @endauth
+              @guest  <!--está atenticado então some o entrar e cadastrar-->
+              <li class="nav-item">
+                <a href="/login" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/register" class="nav-link">Cadastrar</a>
+              </li>
+              @endguest
             </ul>
-        </div>
+          </div>
         </nav>
-        </header>
-        <main>
+      </header>
+      <main>
         <div class="container-fluid">
-        <div class="row">
+          <div class="row">
             @if(session('msg'))
-            <p class="msg">{{ session('msg') }}</p>
+              <p class="msg">{{ session('msg') }}</p>
             @endif
             @yield('content')
+          </div>
         </div>
-        </div>
-        </main>
-        <footer>
+      </main>
+      <footer>
         <p>HDC Events &copy; 2022</p>
-        </footer>
-        <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
-        </body>
+      </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    </body>
 </html>
